a5a22b039fc72aa5fd925c75fb79dd96bb6e80ed
ava-trueworthy <avatrueworthy@gmail.com>
2024-02-14 17:13:57 -0500

    Fixed sensor.py

--- sensor.py
+++ sensor.py
@@ -8,5 +8,6 @@ class SENSOR:
       self.values = numpy.zeros(1000)
 
    def Get_Value(self, time):
-      self.values[time] = pyrosim.Get_Touch_Sensor_Value_For_Link("linkName")
-      print(self.values)
+      self.values[time] = pyrosim.Get_Touch_Sensor_Value_For_Link(self.linkName)
+      if time == 999:
+         print(self.values)
