28a4ed3205d26bd3f619b337f86454bd70d76a7d
ava-trueworthy <avatrueworthy@gmail.com>
2024-03-17 20:12:34 -0400

    Started neural network

--- generate.py
+++ generate.py
@@ -37,6 +37,11 @@ def Generate_Brain():
    pyrosim.Send_Motor_Neuron(name=3, jointName="Torso_BackLeg")
    pyrosim.Send_Motor_Neuron(name=4, jointName="Torso_FrontLeg")
 
+   for i in range(3):
+      for j in range(2):
+         pyrosim.Send_Synapse(sourceNeuronName=i, targetNeuronName=j, weight=1.0)
+
+   """
    pyrosim.Send_Synapse(sourceNeuronName=0, targetNeuronName=3, weight=-0.5)
    pyrosim.Send_Synapse(sourceNeuronName=1, targetNeuronName=3, weight=0.0)
    pyrosim.Send_Synapse(sourceNeuronName=2, targetNeuronName=3, weight=0.0)
@@ -44,6 +49,7 @@ def Generate_Brain():
    pyrosim.Send_Synapse(sourceNeuronName=0, targetNeuronName=4, weight=-1.0)
    pyrosim.Send_Synapse(sourceNeuronName=1, targetNeuronName=4, weight=0.0)
    pyrosim.Send_Synapse(sourceNeuronName=2, targetNeuronName=4, weight=-3.0)
+   """
 
    pyrosim.End()
 
