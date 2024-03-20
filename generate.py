5d7fa0a6edde431b0392c063251bf62a3e589279
ava-trueworthy <avatrueworthy@gmail.com>
2024-03-19 22:21:53 -0400

    Random search

--- generate.py
+++ generate.py
@@ -1,4 +1,5 @@
 import pyrosim.pyrosim as pyrosim
+import random
 
 length = 1
 width = 1
@@ -38,19 +39,18 @@ def Generate_Brain():
    pyrosim.Send_Motor_Neuron(name=4, jointName="Torso_FrontLeg")
 
    for i in range(3):
-      for j in range(2):
-         pyrosim.Send_Synapse(sourceNeuronName=i, targetNeuronName=j, weight=1.0)
+      for j in range(3,5):
+         pyrosim.Send_Synapse(sourceNeuronName=str(i), targetNeuronName=str(j), weight=(random.random()*2)-1)
 
    """
-   pyrosim.Send_Synapse(sourceNeuronName=0, targetNeuronName=3, weight=-0.5)
-   pyrosim.Send_Synapse(sourceNeuronName=1, targetNeuronName=3, weight=0.0)
-   pyrosim.Send_Synapse(sourceNeuronName=2, targetNeuronName=3, weight=0.0)
+   pyrosim.Send_Synapse(sourceNeuronName=0, targetNeuronName=3, weight=random.random())
+   pyrosim.Send_Synapse(sourceNeuronName=1, targetNeuronName=3, weight=random.random())
+   pyrosim.Send_Synapse(sourceNeuronName=2, targetNeuronName=3, weight=random.random())
 
-   pyrosim.Send_Synapse(sourceNeuronName=0, targetNeuronName=4, weight=-1.0)
-   pyrosim.Send_Synapse(sourceNeuronName=1, targetNeuronName=4, weight=0.0)
-   pyrosim.Send_Synapse(sourceNeuronName=2, targetNeuronName=4, weight=-3.0)
-   """
-
+   pyrosim.Send_Synapse(sourceNeuronName=0, targetNeuronName=4, weight=(random.random()*2)-1)
+   pyrosim.Send_Synapse(sourceNeuronName=1, targetNeuronName=4, weight=(random.random()*2)-1)
+   pyrosim.Send_Synapse(sourceNeuronName=2, targetNeuronName=4, weight=(random.random()*2)-1)
+   """      
    pyrosim.End()
 
 Create_World()
