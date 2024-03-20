72713993ec4b67c8c863dd93f7623b69bb607c76
ava-trueworthy <avatrueworthy@gmail.com>
2024-03-19 22:30:09 -0400

    Started hill climber

--- search.py
+++ search.py
@@ -1,8 +1,14 @@
+from hillclimber import HILL_CLIMBER
 import os
 
+"""
 for i in range(5):
 
    os.system("python3 generate.py")
 
    os.system("python3 simulate.py")
+"""
+
+hc = HILL_CLIMBER()
+
 
